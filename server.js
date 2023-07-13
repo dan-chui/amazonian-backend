9e7047c17d9e459da37f7b3f1894e37781f95f2f
Dan Chui <dchui108@gmail.com>
2023-07-13 14:56:21 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+var cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+
+// Create Express server
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Use Middleware cors
+app.use(cors());
+
+// Recommended by Stripe documentation
+app.use(express.static(path.join(__dirname, "build")));
+app.use(express.json());
+
+app.post("/checkout", async (req, res) => {
+  // Initiate session with Stripe
+  const items = req.body.items;
+  let lineItems = [];
+
+  // Create new array in Stripe-format to process payments
+  items.forEach((item) => {
+    lineItems.push({
+      price: item.id,
+      quantity: item.quantity,
+    });
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: "payment",
+    success_url: "http://localhost:5173/success",
+    cancel_url: "http://localhost:5173/cancel",
+  });
+
+  res.send(JSON.stringify({ url: session.url }));
+});
+app.get("/test", (req, res) => {
+  res.send("Api is working");
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/front-end/build/index.html"));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
